3f9d803e58906d411f0f5d037d5d9136400b22aa
Nirmal <nirmalraj1025@gmail.com>
2023-01-29 13:18:30 +0530

    first commit

--- redux/src/Component/Register.js
+++ redux/src/Component/Register.js
@@ -1,23 +1,37 @@
 import React from "react";
 
 function Register() {
-  const handleOnSubmit = () => {
-    console.log("ji");
+  const handleOnSubmit = (value) => {
+    console.log(value);
   };
+
   return (
     <div>
-      <form onSubmit={handleOnSubmit} action="">
+      <form onSubmit={handleOnSubmit}>
         <input
           type="text"
           name="firstName"
           placeholder="Enter your firstName"
+          onChange={(e) => e.target.value}
+          required
         />
-        <input type="text" name="lastName" placeholder="Enter your lastName" />
-        <input type="email" name="email" placeholder="Enter your email" />
         <input
-          type="pasword"
+          type="text"
+          name="lastName"
+          placeholder="Enter your lastName"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          required
+        />
+        <input
+          type="password"
           name="password"
           placeholder="Enter your password"
+          required
         />
         <input type="submit" />
       </form>
